0ed6ada1306178c57c9e3599de594ecb77f17dc9
Mohdzaid Ansari <mohdzaid_ansari@seologistics.com>
2021-10-20 20:52:36 +0530

    a

--- weatherapiapp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url
-from django.contrib import admin
-from .views import GuestViewset,weatherapi
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('guest', GuestViewset, basename='user')
-urlpatterns = router.urls+[url('getweather',weatherapi,name="weatherapi")]
